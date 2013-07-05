db5df3cf7779e0a175c245563baa0572e93df1f7
Howard Mao <zhehao.mao@gmail.com>
2013-07-04 22:41:56 -0700

    add week3 template

---
+++ week3/main.py
@@ -0,0 +1,10 @@
+def euclid(a, b):
+    # place your code here
+
+# The following code will test several a-b pairs to make sure the function
+# you wrote outputs the correct answer.
+# If the answer is wrong, you will see an error message print out when you
+# run the code. Otherwise, the program will print nothing.
+assert euclid(6, 4) == 2
+assert euclid(13, 7) == 1
+assert euclid(48, 72) == 24
