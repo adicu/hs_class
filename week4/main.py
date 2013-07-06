c3863d563acb2ad0f82e0df672241fe26e956c06
Howard Mao <zhehao.mao@gmail.com>
2013-07-06 10:33:20 -0700

    add week4 solutions

--- week4/main.py
+++ week4/main.py
@@ -1,6 +1,15 @@
 # The sieve function should return the list of all prime numbers 
 # less than or equal to n
 def sieve(n):
-    # Put your implementation here
+    primes = list(range(2, n + 1))
+    i = 0
+    while i < (len(primes) - 1):
+        newprimes = primes[:i+1]
+        for n in primes[i+1:]:
+            if n % primes[i] != 0:
+                newprimes.append(n)
+        primes = newprimes
+        i += 1
+    return primes
 
 assert sieve(20) == [2, 3, 5, 7, 11, 13, 17, 19]
