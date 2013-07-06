2fad8767ab84e2b66d7922fb4d09d79e4c53c2f9
Howard Mao <zhehao.mao@gmail.com>
2013-07-06 10:53:48 -0700

    use custom assertEquals function instead of builtin assert

--- week4/main.py
+++ week4/main.py
@@ -1,6 +1,10 @@
+def assertEqual(a, b):
+    if a != b:
+        raise AssertionError("expected " + str(a) + ", got " + str(b))
+
 # The sieve function should return the list of all prime numbers 
 # less than or equal to n
 def sieve(n):
     # Put your implementation here
 
-assert sieve(20) == [2, 3, 5, 7, 11, 13, 17, 19]
+assertEqual(sieve(20), [2, 3, 5, 7, 11, 13, 17, 19])
