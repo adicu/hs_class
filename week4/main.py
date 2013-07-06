164539a50ba60855b7d0ee638875b729d8b18318
Howard Mao <zhehao.mao@gmail.com>
2013-07-06 10:25:54 -0700

    add week4 template

---
+++ week4/main.py
@@ -0,0 +1,6 @@
+# The sieve function should return the list of all prime numbers 
+# less than or equal to n
+def sieve(n):
+    # Put your implementation here
+
+assert sieve(20) == [2, 3, 5, 7, 11, 13, 17, 19]
