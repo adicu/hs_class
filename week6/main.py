83f14cd23436a2c4a691e6f47f1b524dfac7bc5b
Zhehao Mao <zhehao.mao@gmail.com>
2013-07-17 16:56:30 -0700

    add solution to bouncing ball

--- week6/main.py
+++ week6/main.py
@@ -4,16 +4,26 @@ import random
 
 class BouncingBallHandler(pantograph.PantographHandler):
     def setup(self):
-        # This method is run once when the animation starts 
-        # Put any code which sets up the animation here
+        self.x = random.randint(0, self.width)
+        self.y = random.randint(0, self.height)
+        self.xvel = random.randint(1, 5)
+        self.yvel = random.randint(1, 5)
 
     def update(self):
-        # This method is called repeatedly at a fixed interval
-        # Here, you should update the bouncing ball's postion and redraw it
+        if self.x < 0 or self.x > self.width:
+            self.xvel *= -1
+        if self.y < 0 or self.y > self.height:
+            self.yvel *= -1
+
+        self.x += self.xvel
+        self.y += self.yvel
+
+        self.clear_rect(0, 0, self.width, self.height)
+        self.fill_circle(self.x, self.y, 10, "#f00")
 
 
 if __name__ == '__main__':
     app = pantograph.PantographApplication([
-        ("Pantograph", "/", BouncingBallHandler)
+        ("BouncingBall", "/", BouncingBallHandler)
     ])
     app.run()
